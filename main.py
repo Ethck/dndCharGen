9dd33d88b6d63d95bc93d7614bead74b3d7217fd
root <root@localhost.localdomain>
2019-04-01 21:50:40 +0000

    Added some dict functionality

--- main.py
+++ main.py
@@ -22,3 +22,22 @@ def writePDF(inputPath, outputPath, dataDict):
 
 	pdfrw.PdfWriter().write(outputPath, templatePDF)
 
+
+dataDict = {
+	'name': TBD,
+	'str': 10,
+	'dex': 10,
+	'con': 10,
+	'int': 10,
+	'wis': 10,
+	'cha': 10,
+	'strMod': 0,
+	'dexMod': 0,
+	'conMod': 0,
+	'intMod': 0,
+	'wisMod': 0,
+	'chaMod': 0,
+	'class': [['Fighter', 1]],
+	'hp': 10,
+}
+
